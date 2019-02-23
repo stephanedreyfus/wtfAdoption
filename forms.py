af0251aaa8ddff3c7328433e75709347e47d2b23
Stéphane Dryefus <floatingrock@gmail.com>
2019-02-22 16:35:39 -0800

    Add initial pet detail page.

--- forms.py
+++ forms.py
@@ -1,15 +1,19 @@
 from flask_wtf import FlaskForm
-from flask_debugtoolbar import DebugToolbarExtension
-from models import Pet, db, connect_db
 from wtforms.validators import InputRequired, Optional, URL, NumberRange
-from wtforms import StringField, IntegerField, BooleanField, TextAreaField, SelectField
+from wtforms import StringField,
+                    IntegerField,
+                    BooleanField,
+                    TextAreaField,
+                    SelectField
 
 
 class AddPet(FlaskForm):
     ''' Form for adding pets '''
 
     name = StringField('Pet Name', validators=[InputRequired()])
-    species = SelectField('Species', choices=[("cat", "Cat"), ("dog", "Dog"), ("porcupine", "Porcupine")])
+    species = SelectField('Species', choices=[("cat", "Cat"), ("dog", "Dog"),
+                          ("porcupine", "Porcupine")])
     photo_url = StringField('Photo URL', validators=[URL(), Optional()])
-    age = IntegerField('Age', validators=[InputRequired(), NumberRange(min=0, max=30)])
+    age = IntegerField('Age', validators=[InputRequired(),
+                       NumberRange(min=0, max=30)])
     notes = TextAreaField('Notes')
